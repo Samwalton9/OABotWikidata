3976952c79a21843c09057cc56114433bebedf6e
Sam Walton <samwalton9@gmail.com>
2018-11-29 14:47:04 -0800

    Creating sanity check functions file skeleton

---
+++ sanity_checks.py
@@ -0,0 +1,9 @@
+def valid_doi(doi):
+    # Input: A DOI string
+    # Output: True or False for match against DOI regex
+    pass
+
+def sensible_url(url):
+    # Input: A URL string
+    # Output: True or False for match against URL sanity checks (regex?)
+    pass
